1f25171ab8029ecaca0cf46bc985fdae80defa58
farhanshahriyar <farhan.abir9999@gmail.com>
2024-02-02 19:21:50 +0600

    setted up authprovider

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,21 +1,99 @@
 /* eslint-disable react/prop-types */
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
 
-export const AuthContext = createContext()
-const AuthProvider = ({children}) => {
-   const [user, setUser] = useState(null)
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  // create an account via email and password
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
 
-    const authInfo = {
-      user,
-      
-    }
+  // sign in email and password with gmail
+  const signUpWithGmail = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // login using email and password
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
+  // logout functionalities
+  const logout = () => {
+    signOut(auth);
+  };
+
+  // update profile
+  const updateProfile = ({ name, photoURL }) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photoURL,
+    });
+  };
+
+  // check signed in-users
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+        setLoading(false);
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    createUser,
+    signUpWithGmail,
+    login,
+    logout,
+    updateProfile,
+  };
 
   return (
-    <AuthContext.Provider value={authInfo }>
-        {children}
+    <AuthContext.Provider value={authInfo}>
+      {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
-export default AuthProvider
+export default AuthProvider;
